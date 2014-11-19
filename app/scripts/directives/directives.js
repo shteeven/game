1a10de2b29f6d7651af68a5f2d1bd3a1c382e030
Steven Barnhurst <stevenbarnhurst@gmail.com>
2014-11-19 10:41:35 +0900

    Create a working level selector and player stats generator

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -8,11 +8,18 @@ app.directive('level', function() {
   return {
     restrict: 'AE',
     replace:true,
-    scope:{level:'='},
-    template: '<div><select ng-model="level" ng-options="level for level in range">'+
+    scope:{playerStats:'='},
+    template: '<div><select ng-model="level" ng-change="update(level)" ng-options="level for level in range">'+
       '<option value="">Choose your level:</option> <!-- default --></select></div>',
-    link:function(scope){
-      scope.range = [1,2,3,4,5];
+    link:function($scope){
+      $scope.range = [1,2,3,4,5];
+      $scope.playerStats = {level: 0, health:0, attack:function(){return null}};
+      $scope.update = function(val){
+        $scope.level = val;
+        $scope.playerStats.level = val;
+        $scope.playerStats.health = val*10+50;
+        $scope.playerStats.attack = function(){return (val*5+20)*Math.random();};
+      };
     }
   };
 });
@@ -23,8 +30,7 @@ app.directive('player', function() {
     scope:{ player:'=' },
     templateUrl: 'views/templates/player.html',
     link:function(scope){
-      scope.player = {level:0, health:0, attack:function(){return (this.level*5+20)*Math.random();}};
-      scope.player.health = scope.player.level*10+50;
+
       scope.report = function(message){
         console.log(message);
       };
