f34d49e839e0c3122a7a212952921fa7566f2c8c
Neven Hajder <neven.hajder@gmail.com>
2014-11-26 21:31:06 +0900

    Add modal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,51 +9,91 @@
  */
 var app = angular.module('tggApp');
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.gameStarted = false;
+app.controller('MainCtrl', ['$scope', '$modal',
+    function ($scope, $modal) {
+        $scope.gameStarted = false;
 
-  function startGame(){
-    $scope.gameStarted = true;
-    nextTurn();
-  }
-  function resetGame(){
-    $scope.player1Reset();
-    $scope.player2Reset();
-    $scope.gameStarted = false;
-    $scope.offense = undefined;
-    $scope.defense = undefined;
-    $scope.winner = undefined;
-  }
-  function nextTurn(){
-    if (!$scope.offense){
-      if (Math.random() >= 0.5){
-        $scope.offense = $scope.player1;
-        $scope.defense = $scope.player2;
-      }else{
-        $scope.offense = $scope.player2;
-        $scope.defense = $scope.player1;
-      }
-    }else{
-      var placeHolder = $scope.defense;
-      $scope.defense = $scope.offense;
-      $scope.offense = placeHolder;
-    }
-  }
-  function executeAttack(){
-    $scope.defense.attacked($scope.offense.attack());
-    if ($scope.defense.health <= 0) {
-      $scope.winner = $scope.offense;
-      $scope.loser = $scope.defense;
-      $scope.defense.health = 0;
-    }else{
-      nextTurn();
-    }
-  }
+        function startGame() {
+            $scope.gameStarted = true;
+            nextTurn();
+        }
 
-  $scope.startGame = startGame;
-  $scope.resetGame = resetGame;
-  $scope.executeAttack = executeAttack;
+        function resetGame() {
+            $scope.player1Reset();
+            $scope.player2Reset();
+            $scope.gameStarted = false;
+            $scope.offense = undefined;
+            $scope.defense = undefined;
+            $scope.winner = undefined;
+        }
+
+        function nextTurn() {
+            if (!$scope.offense) {
+                if (Math.random() >= 0.5) {
+                    $scope.offense = $scope.player1;
+                    $scope.defense = $scope.player2;
+                } else {
+                    $scope.offense = $scope.player2;
+                    $scope.defense = $scope.player1;
+                }
+            } else {
+                var placeHolder = $scope.defense;
+                $scope.defense = $scope.offense;
+                $scope.offense = placeHolder;
+            }
+        }
+
+        function executeAttack() {
+            $scope.defense.attacked($scope.offense.attack());
+            if ($scope.defense.health <= 0) {
+                $scope.winner = $scope.offense;
+                $scope.loser = $scope.defense;
+                $scope.defense.health = 0;
+                openModal();
+            } else {
+                nextTurn();
+            }
+        }
+
+        function openModal() {
+            var modalInstance = $modal.open({
+                templateUrl: 'views/templates/winnerModal.html',
+                controller: 'ModalInstanceCtrl',
+                resolve: {
+                    item: function () {
+                        return resetGame;
+                    },
+                    winner: function () {
+                        return $scope.winner;
+                    }
+                }
+            });
+            modalInstance.result.then(function (runFunction) {
+                runFunction();
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        }
+
+        $scope.openModal = openModal;
+        $scope.startGame = startGame;
+        $scope.resetGame = resetGame;
+        $scope.executeAttack = executeAttack;
 
 
 
 }]);
+
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, item, winner) {
+
+    $scope.item = item;
+    $scope.winner = winner;
+    $scope.ok = function () {
+        $modalInstance.close($scope.item);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
